26629b342996a98c94809644bdc291ca9431ed99
TRY-ER <avyarth00@gmail.com>
2022-05-13 14:16:06 +0530

    In this commit created another script and efficiently ran and converted the  synthetic data generated from SMOTENC

--- src/syn_data_gen.py
+++ src/syn_data_gen.py
@@ -13,7 +13,7 @@ main_df = pd.read_csv("../inputs/standard_ml_preprocessed_df.csv")
 y_str = "PCE_categorical"
 y_int = "JV_default_PCE_numeric"
 
-drop_vals = ["JV_average_over_n_number_of_cells_numeric"]
+# drop_vals = ["JV_average_over_n_number_of_cells_numeric"]
 
 # destructured the target column  
 main_df[y_str] = main_df[y_str] * 10 
@@ -48,8 +48,8 @@ Y = use_df[y_str]
 print(f" shape of Y :  {Y.shape}")
 Y_int = use_df[y_int]
 
-use_df = use_df.drop([y_str, y_int], axis =1)
-use_df = use_df.drop(drop_vals, axis =1)
+use_df = use_df.drop([y_str], axis =1)
+# use_df = use_df.drop(drop_vals, axis =1)
 
 X = use_df.copy()
 print(f" shape of X :  {X.shape}")
@@ -60,8 +60,8 @@ cat_cols = []
 for key,value in col_data.items():
     if key == "categorical":
         for val in value:
-            if val not in drop_vals:
-                cat_cols.append(val)
+            # if val not in drop_vals:
+            cat_cols.append(val)
 cat_col_int = []
 index =0
 for value in X.columns:
@@ -69,6 +69,8 @@ for value in X.columns:
         if cat == value: cat_col_int.append(index)
     index += 1
 
+for ind in cat_col_int:
+    print(X.columns[ind])
 print(f" Length of categorcal indicies : {len(cat_col_int)}")
 smote = SMOTENC(categorical_features=cat_col_int,
                 sampling_strategy="minority",
@@ -81,17 +83,17 @@ print(f" shape of input dataset {main_df.shape}")
 print(f" shape of syn generated X dataset {x_smoted.shape}")
 print(f" shape of syn generated y dataset {y_smoted.shape}")
 
-x_smoted["target_cat"] = y_smoted
+x_smoted["tar_col"] = y_smoted
 # structuring the target column
-x_smoted["target_cat"] = x_smoted["target_cat"] / 10 
-x_smoted["target_cat"] = x_smoted["target_cat"].astype(np.float64)
+x_smoted["tar_col"] = x_smoted["tar_col"] / 10 
+x_smoted["tar_col"] = x_smoted["tar_col"].astype(np.float64)
 
 export_df = x_smoted.copy()
 # print(export_df.tail(10))
 
 try:
     print(f" Exporting the data set at :: ../outputs/data/smoted_scaled_data.csv")
-    export_df.to_csv("../outputs/data/smoted_scaled_data.csv")
+    export_df.to_csv("../outputs/data/smoted_scaled_data.csv",index=False)
     print(" Export Sucessful")
 except:
     print(" Couldn't export the result dataframe")
